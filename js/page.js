9e21cf2d1e0a9363cfbec07cf8ef1cdf5c1a3d87
Ajay <sixpintflagon@yahoo.com>
2018-07-18 22:14:41 -0500

    Make more IE-friendly
    Removes template literals so that the page will load correctly on IE (newer versions, at any rate).

--- js/page.js
+++ js/page.js
@@ -1,25 +1,21 @@
 Vue.component('p-artifact', {
   props: ['type'],
-  template: `
-    <div class="column">
-      <div class="card">
-        <div class="card-content">
-            <slot></slot>
-        </div>
-      </div>
-    </div>`
+  template: '\
+    <div class="column">\
+      <div class="card">\
+        <div class="card-content">\
+            <slot></slot>\
+        </div>\
+      </div>\
+    </div>'
 });
 
 Vue.component('section-title', {
-  template: `
-    <div class="container">
-      <h1 class="title"><slot></slot></h1>
-    </div>
-  `
-});
-
-Vue.component('experience', {
-  template: ``
+  template: '\
+    <div class="container">\
+      <h1 class="title"><slot></slot></h1>\
+    </div>\
+  '
 });
 
 vm = new Vue({
